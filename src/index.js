fdd3fa9fd21909a0d1ad8275e9f013b52aa7d82b
alexeyvax <alexeyvax@mail.ru>
2017-06-07 13:20:49 +0300

    remove folder scripts

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './components/Routes.jsx';
-const data = window.__PRELOADED_STATE__.replace(/&quot;/g, '"') || []; // Использовать при handlebars
-const listBooks = JSON.parse(data.replace(/^["']|["']$/g, '')) || []; // обрезаю кавычки в начале и в конце
+const data = window.__PRELOADED_STATE__.replace(/&quot;/g, '"') || []; // use with handlebars
+const listBooks = JSON.parse(data.replace(/^["']|["']$/g, '')) || []; // cut quotes
 
 ReactDOM.render(
 	<Routes listBooks={listBooks} />,
